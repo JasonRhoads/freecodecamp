176c21ce2504803600af22af3445b7a13e53e40d
Jason Rhoads <jasrhoads@gmail.com>
2024-02-09 16:16:54 -0800

    create image slider component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import Accordian from './components/accordian';
+import ImageSlider from './components/image-slider';
 import RandomColor from './components/random-color';
 import StarRating from './components/star-rating';
 
@@ -13,8 +14,14 @@ function App() {
       {/* Random Color Component */}
       {/* <RandomColor /> */}
 
-      {/* Star Rating */}
-      <StarRating noOfStars={10}/>
+      {/* Star Rating Component*/}
+      {/* <StarRating noOfStars={10}/> */}
+
+      {/* Image slider Component*/}
+      <ImageSlider 
+        url={"https://picsum.photos/v2/list"} 
+        page={"1"} 
+        limit={"10"}/>
     </div>
   );
 }
