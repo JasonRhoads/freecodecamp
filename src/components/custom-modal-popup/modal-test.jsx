08da2eecdbd340ae6e4c8d4d530938444186f40d
Jason Rhoads <jasrhoads@gmail.com>
2024-02-15 13:52:32 -0800

    add custom modal popup component

---
+++ src/components/custom-modal-popup/modal-test.jsx
@@ -0,0 +1,34 @@
+import { useState } from "react"
+import Modal from "./modal"
+import "./modal.css"
+
+
+export default function ModalTest() {
+
+    const [showModalPopup, setShowModalPopup] = useState(false);
+
+
+    function handleToggleModalPopup() {
+        setShowModalPopup(!showModalPopup)
+    }
+
+    function onClose() {
+        setShowModalPopup(false)
+    }
+
+    return (
+        <div>
+            <button onClick={handleToggleModalPopup}>Open Modal Popup</button>
+            {
+                showModalPopup && (
+                <Modal 
+                onClose={onClose}
+                id={'custom-id'}
+                header={<h1>Customized Header</h1>}
+                body={<div>Customized body</div>} 
+                footer={<h1>Customized Footer</h1>}
+                />)
+            }
+        </div>
+    )
+}
